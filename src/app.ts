423ff8f68ec02d59360c852f7fd47441d99c9ab2
Nimesh Dhumal <nimeshdhumal458@gmail.com>
2026-03-10 21:29:25 +0530

    PORT number is changed from 3000 to 5000.

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ import errorHandler from './middlewares/error.handler';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ dotenv.config(); // Load .env first before anything else
 
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
